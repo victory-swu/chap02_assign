6d9a8e4ebdd89dfb4a26e454a65f0f16dd5d2921
user <user@DESKTOP-JUP0A8G>
2025-04-01 02:58:45 +0900

    Assignments

--- Assignment05/assign05.c
+++ Assignment05/assign05.c
@@ -1,8 +1,28 @@
-#define
-_CRT_SECURE_NO_WARNINGS
+/*
+	* 파일명: assign05.c
+
+	* 내용: PA05. 몸무게를 입력받아서 출력하는 프로그램을 작성하시오. (몸무게는 실수로 입력받으며, 소수점 이하 2자리까지만 출력한다.)
+
+	* 작성자: 임승리
+
+	* 날짜: 2025.4.1
+
+	* 버전: v1.0
+
+  */
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+void Weight();
+
 int main()
+{
+	Weight();
+
+	return 0;
+}
+
+void Weight()
 {
 	float weight;
 
